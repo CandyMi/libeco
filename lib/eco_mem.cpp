468d600ef2d5d0a3ab9e77ec98ca6f6fb23d6081
CandyMi <869646063@qq.com>
2021-11-05 11:24:13 +0800

    完善memory hook的代码

--- lib/eco_mem.cpp
+++ lib/eco_mem.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
-#include <execinfo.h>
 
 #include "eco.hpp"
 #include "eco_event.hpp"
 
-#define BACKTRACE_SIZE   (32)
-void *buffer[BACKTRACE_SIZE];
-
 /* NEW */
-
-void* operator new(size_t size) throw(std::bad_alloc)
+void* operator new(size_t size) noexcept(false)
 {
-#ifdef ECO_PRINTSTACK
-  int nptrs = backtrace(buffer, BACKTRACE_SIZE);
-  char **strings = backtrace_symbols(buffer, nptrs);
-  if (strings){
-    std::cout<< "`new`试图申请: " << size << "字节." << std::endl;
-    for (int j = 0; j < nptrs; j++)  
-      printf("  [%02d] %s\n", j, strings[j]);
-  }
-#endif
   if (size < 1)
     throw std::bad_alloc();
   /* malloc */
@@ -29,17 +15,8 @@ void* operator new(size_t size) throw(std::bad_alloc)
   return ptr;
 }
 
-void* operator new[](size_t size) throw(std::bad_alloc)
+void* operator new[](size_t size) noexcept(false)
 {
-#ifdef ECO_PRINTSTACK
-  int nptrs = backtrace(buffer, BACKTRACE_SIZE);
-  char **strings = backtrace_symbols(buffer, nptrs);
-  if (strings){
-    std::cout<< "`new[]`试图申请: " << size << "字节." << std::endl;
-    for (int j = 0; j < nptrs; j++)  
-      printf("  [%02d] %s\n", j, strings[j]);
-  }
-#endif
   if (size < 1)
     throw std::bad_alloc();
   /* malloc */
@@ -50,34 +27,15 @@ void* operator new[](size_t size) throw(std::bad_alloc)
 }
 
 /* Delete */
-
-void operator delete(void* ptr) throw()
+void operator delete(void* ptr) noexcept(true)
 {
-#ifdef ECO_PRINTSTACK
-  int nptrs = backtrace(buffer, BACKTRACE_SIZE);
-  char **strings = backtrace_symbols(buffer, nptrs);
-  if (strings){
-    std::cout<< "`delete`试图释放内存"<< std::endl;
-    for (int j = 0; j < nptrs; j++)  
-      printf("  [%02d] %s\n", j, strings[j]);
-  }
-#endif
   if (!ptr)
     return;
   return eco_free(ptr);
 }
 
-void operator delete[] (void* ptr) throw()
+void operator delete[] (void* ptr) noexcept(true)
 {
-#ifdef ECO_PRINTSTACK
-  int nptrs = backtrace(buffer, BACKTRACE_SIZE);
-  char **strings = backtrace_symbols(buffer, nptrs);
-  if (strings){
-    std::cout<< "`delete[]`试图释放内存"<< std::endl;
-    for (int j = 0; j < nptrs; j++)  
-      printf("  [%02d] %s\n", j, strings[j]);
-  }
-#endif
   if (!ptr)
     return;
   return eco_free(ptr);
